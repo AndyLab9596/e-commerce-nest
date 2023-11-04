b6d71ef10fc8aa067792a4094e502368f6a234e3
vyct1 <thienvy95@gmail.com>
2023-11-04 14:49:44 +0700

    create product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,39 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNotEmpty()
+  @IsNumber(
+    { maxDecimalPlaces: 2 },
+    { message: 'price should be number & max decimal precision 2' },
+  )
+  @IsPositive()
+  price: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  stock: number;
+
+  @IsNotEmpty()
+  @IsArray()
+  images: string[];
+
+  @IsNotEmpty()
+  @IsNumber()
+  categoryId: number;
+}
