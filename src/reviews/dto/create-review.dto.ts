46097748cdd123180287dde82c1d5cd97591f91c
vyct1 <thienvy95@gmail.com>
2023-11-06 06:02:19 +0700

    create-review dto

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,15 @@
-export class CreateReviewDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateReviewDto {
+  @IsNotEmpty()
+  @IsNumber()
+  productId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  ratings: number;
+
+  @IsNotEmpty()
+  @IsString()
+  comment: string;
+}
